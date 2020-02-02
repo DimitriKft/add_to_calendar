a5ae393634afdc2785e0cf220da9deffe4ed8357
klopfstein <dimitri.kft@gmail.com>
2020-02-02 22:47:02 +0100

    add url for google calendar

--- tmpl/value_default_dropdown.php
+++ tmpl/value_default_dropdown.php
@@ -1,6 +1,29 @@
 <?php
 
 $n = 0;
+
+// var_dump($this->field);        // liste       object
+// var_dump($this->values);       // array       empty 
+// var_dump($this->field->value); // array       empty
+// var_dump($field);              // intersting, this variable works like $this->field !
+// var_dump($this->item);         // i           have my       value, she this is "public "fieldvalues=>array()...
+var_dump($item->fieldvalues);
+$title_test =  $item->fieldvalues[21][0];
+$desc_test  =  $item->fieldvalues[26][0];
+$date_start =  $item->fieldvalues[24][0];
+$date_end   =  $item->fieldvalues[25][0];
+$pattern = array("-", ":");
+$date_start = str_replace($pattern, "","$date_start");
+$date_start = str_replace(" ", "T",$date_start);
+echo $date_start . '<br>';
+$date_end = str_replace($pattern, "","$date_end");
+$date_end = str_replace(" ", "T",$date_end);
+echo $date_end;
+// echo $item->fieldvalues[21][0] . '<br>';
+// echo $item->fieldvalues[26][0] . '<br>';
+// echo $item->fieldvalues[24][0] . '<br>';
+// echo $item->fieldvalues[25][0] . '<br>';
+echo '<a href="https://www.google.com/calendar/render?action=TEMPLATE&text=' . $item->fieldvalues[21][0] . '&dates=' . $date_start . '00Z/' .$date_end   . '00Z&ctz=Europe/Paris&details=' . $desc_test . '&location=paris,%20france">LIEN GOOGLE CALENDAR</a>';
 foreach ($values as $value)
 {
 	// Skip empty value, adding an empty placeholder if field inside in field group
