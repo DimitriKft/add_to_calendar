52cc6508beb99234344d9803831e0e060f173823
klopfstein <dimitri.kft@gmail.com>
2020-02-05 15:12:50 +0100

    url ootlook ok

--- tmpl/value_default_dropdown.php
+++ tmpl/value_default_dropdown.php
@@ -6,10 +6,20 @@
 	// var_dump($values); // test $values , is empty array
 
 
+	// $json = ($item->fieldvalues['23']);
+	// $encode = json_encode($json, true);
+	// var_dump($encode);
+	// var_dump(json_decode($encode));
+	// var_dump($json);
+
+
+
 $n = 0;
 
-var_dump($n);
+// $values = $field->parameters;
 
+
+// var_dump($values);
 foreach ($values as $value)
 {
 	// Skip empty value, adding an empty placeholder if field inside in field group
@@ -21,12 +31,13 @@ foreach ($values as $value)
 				}
 				continue;
 			}
-			
+			$pretext = '<h1>';
+			$posttext = '</h1>';
 			// Add prefix / suffix
 			$field->{$prop}[$n]	= $pretext . $value . $posttext;
-				
+
 			// Add microdata to every value if field -- is -- in a field group
-	if ($is_ingroup && $itemprop) $field->{$prop}[$n] = '<div style="display:inline" itemprop="'.$itemprop.'" ><h1>TOTOTOTOTO</h1>' .$field->{$prop}[$n]. '</div>';
+	if ($is_ingroup && $itemprop) $field->{$prop}[$n] = 'TOTO<div style="display:inline" itemprop="'.$itemprop.'" ><h1>TOTOTOTOTO</h1>' .$field->{$prop}[$n]. '</div>';
 	
 	$n++;
 	if (!$multiple) break;  // multiple values disabled, break out of the loop, not adding further values even if the exist
